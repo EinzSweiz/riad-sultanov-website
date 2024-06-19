b37de657e73450cfde71d8e427b1f919d86731b1
EinzSweiz <122094020+EinzSweiz@users.noreply.github.com>
2024-06-19 05:31:49 +0000

    Created first page added Bootstrap and created API for JOBS created

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, json, jsonify, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id":1,
+        "title": "Data Analyst",
+        "location": "Baku, Azerbaijan",
+        "salary": "1000$"
+    },
+    {
+        "id":2,
+        "title": "Frontend Developer",
+        "location": "Baku, Azerbaijan",
+        "salary": "1500$"
+    },
+    {
+        "id":3,
+        "title": "Backend Developer",
+        "location": "NewYork, USA",
+        "salary": "2000$"
+    }
+]
 @app.route('/')
-
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True)
